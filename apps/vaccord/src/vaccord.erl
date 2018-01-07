aa8cf51e885a01d1f9a08530c3a4bc55f6cc6335
Hisham Ismail <mhishami@gmail.com>
2018-01-07 23:48:40 +0800

    Added copyrights, vaccord gen_server

--- apps/vaccord/src/vaccord.erl
+++ apps/vaccord/src/vaccord.erl
@@ -21,14 +21,49 @@
 %% SOFTWARE.
 %%
 %%
--module(vaccount_app).
--behaviour(application).
+-module(vaccord).
+-behaviour(gen_server).
+-author('Hisham Ismail <mhishami@gmail.com>').
+-include ("vaccord.hrl").
 
--export([start/2]).
--export([stop/1]).
+%% API.
+-export([start_link/0]).
 
-start(_Type, _Args) ->
-  vaccount_sup:start_link().
+%% gen_server.
+-export([init/1]).
+-export([handle_call/3]).
+-export([handle_cast/2]).
+-export([handle_info/2]).
+-export([terminate/2]).
+-export([code_change/3]).
 
-stop(_State) ->
-  ok.
+-record(state, {
+}).
+
+-define (SERVER, ?MODULE).
+
+%% API.
+
+-spec start_link() -> {ok, pid()}.
+start_link() ->
+	gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%% gen_server.
+
+init([]) ->
+	{ok, #state{}}.
+
+handle_call(_Request, _From, State) ->
+	{reply, ignored, State}.
+
+handle_cast(_Msg, State) ->
+	{noreply, State}.
+
+handle_info(_Info, State) ->
+	{noreply, State}.
+
+terminate(_Reason, _State) ->
+	ok.
+
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
